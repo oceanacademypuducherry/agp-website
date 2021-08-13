92988c9c888ab6fb6f0846d5835bfda60a243577
THAMIZH <thamizharasan2373@gmail.com>
2021-08-13 21:09:00 +0530

    firebase configration successfully completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "./firebase";
 
 function App() {
+  let firestore = firebase.firestore();
+  let userData = firestore.collection("thamizhTest");
+  function getData() {
+    let items = [];
+    userData.onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data().test);
+      });
+    });
+  }
+  getData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDXC3pM7s6ZDAtq8BtgJGbWvhtvFTW61B8",
+  authDomain: "astrology-7cec1.firebaseapp.com",
+  projectId: "astrology-7cec1",
+  storageBucket: "astrology-7cec1.appspot.com",
+  messagingSenderId: "165698073340",
+  appId: "1:165698073340:web:8c1f8b67110be2bfd45d79",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
