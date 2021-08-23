da88e6eb6ca794074184a9c166953a574cd18f9d
THAMIZH <thamizharasan2373@gmail.com>
2021-08-23 13:52:27 +0530

    test firebase hostiong

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <div className="App">
         <TopNavbar />
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + "/"} component={Main} />
+          <Route exact path={"/agp-website"} component={Main} />
           <Route
             path={"/agp-website/book-preview/:id"}
             component={BookPreview}
